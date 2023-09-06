db8ed2ff88cd40e2acab727069526cb4b8dc9c29
hernancontigiani <hernan4790@gmail.com>
2023-09-05 23:42:16 -0300

    Primera migracion

--- back/core/models.py
+++ back/core/models.py
@@ -2,4 +2,16 @@ from django.db import models
 
 # Create your models here.
 
+class Comprobante(models.Model):
+    # Campo para almacenar el archivo del comprobante
+    archivo = models.FileField(upload_to='comprobantes/')
 
+    # Campos de control de auditoría
+    creado_en = models.DateTimeField(auto_now_add=True)
+    actualizado_en = models.DateTimeField(auto_now=True)
+
+
+class Cuota(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    numero = models.IntegerField()
+    challenge = models.ForeignKey(Comprobante, on_delete=models.SET_NULL, blank=True, null=True)
